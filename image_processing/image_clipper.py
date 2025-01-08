6d1cf7bffa7cb54a4e2fb3d677e19790e8567638
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-01-08 15:57:24 +0100

    image_clipper 2.0

--- image_processing/image_clipper.py
+++ image_processing/image_clipper.py
@@ -1,13 +1,14 @@
+import pathlib
+
 import numpy as np
 from PIL import Image
 from transformers import pipeline
 
+pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+
 
 def crop_with_mask_and_resize(image_path):
     # create the pipeline
-    pipe = pipeline(
-        "image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True
-    )
 
     # Charger l'image
     image = Image.open(image_path)
@@ -20,11 +21,32 @@ def crop_with_mask_and_resize(image_path):
     mask_array = np.array(pillow_mask)
 
     # Trouver les indices actifs
-    rows, cols = np.where(mask_array > 0)
+    rows, cols = np.sum(mask_array, axis=1), np.sum(mask_array, axis=0)
+
+    rows, cols = np.where(rows > len(cols) / 10)[0], np.where(cols > len(rows) / 10)[0]
+
+    # Trouver la séquence la plus longue d'indices non interrompue
+    def longest_non_interrupted_sequence(indices):
+        max_sublist = [indices[0]]
+        sublist = [indices[0]]
+        for i in range(1, len(indices)):
+            if int(indices[i]) == int(indices[i - 1]) + 1:
+                sublist.append(int(indices[i]))
+            else:
+                if len(sublist) > len(max_sublist):
+                    max_sublist = sublist
+                sublist = [int(indices[i])]
+        if len(sublist) > len(max_sublist):
+            max_sublist = sublist
+
+        return max_sublist
+
+    longest_rows_sequence = longest_non_interrupted_sequence(rows)
+    longest_cols_sequence = longest_non_interrupted_sequence(cols)
 
     # Calculer les limites du masque
-    min_row, max_row = rows.min(), rows.max()
-    min_col, max_col = cols.min(), cols.max()
+    min_row, max_row = longest_rows_sequence[0], longest_rows_sequence[-1]
+    min_col, max_col = longest_cols_sequence[0], longest_cols_sequence[-1]
 
     # Calculer la largeur et la hauteur
     height = max_row - min_row + 1
@@ -46,11 +68,23 @@ def crop_with_mask_and_resize(image_path):
     cropped_image = pillow_image.crop((min_col, min_row, max_col, max_row))
     # Créer une nouvelle image avec un fond blanc
     final_image = Image.new(
-        "RGB", (cropped_image.width, cropped_image.height), (255, 255, 255)
+        "RGB",
+        (
+            max(cropped_image.width, cropped_image.height),
+            max(cropped_image.width, cropped_image.height),
+        ),
+        (255, 255, 255),
     )
 
     # Coller l'image recadrée sur le fond blanc
-    final_image.paste(cropped_image, (0, 0), cropped_image)
+    if cropped_image.width > cropped_image.height:
+        h = (final_image.height - cropped_image.height) // 2
+        w = 0
+    else:
+        w = (final_image.width - cropped_image.width) // 2
+        h = 0
+
+    final_image.paste(cropped_image, (w, h), cropped_image)
 
     return final_image.resize((256, 256))
 
@@ -58,8 +92,17 @@ def crop_with_mask_and_resize(image_path):
 if __name__ == "__main__":
 
     # Utilisation
-    image_path = "data/test_image_headmind/IMG_6875.jpg"  # Chemin de votre image
-    cropped_image = crop_with_mask_and_resize(image_path)
+    file_path = pathlib.Path(__file__).resolve()
+    main_dir = file_path.parent.parent
+    image_dir_path = main_dir / "data/test_image_headmind/"
+    image_dir = list(image_dir_path.glob("*.jpg"))
 
-    # Afficher ou sauvegarder le résultat
-    cropped_image.show()  # Affiche l'image recadrée
+    # image_path = "data/test_image_headmind/IMG_6934.jpg"
+    # crop_with_mask_and_resize(image_path).show()
+
+    for img_path in reversed(image_dir):
+        image_path = str(img_path)
+        cropped_image = crop_with_mask_and_resize(image_path)
+
+        # Afficher ou sauvegarder le résultat
+        cropped_image.show(title=image_path.split("/")[-1])  # Affiche l'image recadrée
