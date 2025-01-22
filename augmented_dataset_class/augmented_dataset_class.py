714069bdd4a6a835567f837872fbb30e304a3262
Marc Garcia <marccesar.garcia@gmail.com>
2025-01-22 14:40:05 +0100

    test avec 696 classes

---
+++ augmented_dataset_class/augmented_dataset_class.py
@@ -0,0 +1,78 @@
+import pathlib
+import random
+import os
+import cv2
+import datasets
+import numpy as np
+import PIL
+import PIL.Image
+import torch
+
+from .augmentation import process_and_augment
+
+seed = 1
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+
+class CustomDataset(datasets.GeneratorBasedBuilder):
+    def __init__(self, *args, num_augments, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.num_augments = num_augments
+        self.labels = self.create_labels_dict("cropped-dataset-class/articles_train")
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description="Data augmented and cropped images of the dataset",
+            features=datasets.Features(
+                {
+                    "image": datasets.Image(),
+                    "label": datasets.Value("int32"),
+                }
+            ),
+            supervised_keys=None,
+            homepage="---",
+            citation="---",
+        )
+
+    def _split_generators(self, dl_manager):
+        data_dir = pathlib.Path("/home/marc/Challenge/challenge-etude-de-cas/cropped-dataset-class")
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": data_dir / "articles_train", "split": "train"},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"filepath": data_dir / "articles_test", "split": "test"},
+            ),
+        ]
+
+    def _generate_examples(self, filepath: pathlib.Path, split):
+        if split == "train":
+            augment_func = lambda x, y: process_and_augment(x, y, self.num_augments)
+        elif split == "test":
+            augment_func = lambda x, y: [x]
+        else:
+            raise ValueError(f"Unknown name for split: {split}")
+        i = 0
+        for label, label_idx in self.labels.items():
+            for image_file in (filepath / label).glob("*"):
+                image_pil = PIL.Image.open(image_file)
+                image_np_rgb = cv2.cvtColor(np.array(image_pil), cv2.COLOR_BGR2RGB)
+
+                for image in augment_func(image_pil, image_np_rgb):
+                    yield i, {
+                        "image": image,
+                        "label": label_idx,
+                    }
+                    i += 1
+
+    def create_labels_dict(self, base_path):
+        # Get all subfolders (classes) in the base path
+        classes = sorted([folder for folder in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, folder))])
+        
+        # Create labels_dict mapping folder names to unique integers
+        labels_dict = {class_name: idx for idx, class_name in enumerate(classes)}
+        return labels_dict
---
+++ marc/second_classifier.py
@@ -0,0 +1,140 @@
+import os
+import numpy as np
+import torch
+from datasets import load_dataset
+from evaluate import load
+from transformers import (
+    Trainer,
+    TrainingArguments,
+    ViTForImageClassification,
+    ViTImageProcessor,
+    MobileNetV2ForImageClassification,
+    MobileNetV2ImageProcessor
+)
+from pathlib import Path
+
+class PipelineTrain:
+    def __init__(self, model_name, dataset_path, labels_dict, outputdir):
+        self.model_name = model_name
+        self.dataset_path = dataset_path
+        self.outputdir = outputdir
+        self.labels_dict = self.create_labels_dict(labels_dict)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.processor = self.use_processor()
+
+    def use_processor(self):
+        if self.model_name == "google/vit-base-patch16-224-in21k":
+            return ViTImageProcessor.from_pretrained(self.model_name)
+        elif self.model_name == "google/mobilenet_v2_1.0_224":
+            return  MobileNetV2ImageProcessor.from_pretrained(self.model_name)
+        
+    def create_labels_dict(self, base_path):
+        # Get all subfolders (classes) in the base path
+        classes = sorted([folder for folder in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, folder))])
+        
+        # Create labels_dict mapping folder names to unique integers
+        labels_dict = {class_name: idx for idx, class_name in enumerate(classes)}
+        return labels_dict
+
+    def transform(self, example_batch):
+        inputs = self.processor(
+            images=example_batch["image"],
+            return_tensors="pt",
+            size={"height": 224, "width": 224},
+        )
+        inputs["labels"] = torch.tensor(example_batch["label"], dtype=torch.long)
+        return {k: v.to(self.device) for k, v in inputs.items()}
+    
+    def collate_fn(self, batch):
+        pixel_values = torch.stack([x["pixel_values"] for x in batch])
+        labels = torch.tensor([x["labels"] for x in batch])
+        return {"pixel_values": pixel_values, "labels": labels}
+    
+    def create_augmented_dataset(self):
+        return load_dataset(str(self.dataset_path), trust_remote_code=True, num_augments=3)
+    
+    def prepare_model(self):
+        self.id2label = {str(idx): label for label, idx in self.labels_dict.items()}
+        self.label2id = {label: str(idx) for label, idx in self.labels_dict.items()}
+        if self.model_name == "google/vit-base-patch16-224-in21k":
+            self.model = ViTForImageClassification.from_pretrained(
+                self.model_name,
+                num_labels=len(self.labels_dict),
+                id2label=self.id2label,
+                label2id=self.label2id,
+            ).to(self.device)
+        elif self.model_name == "google/mobilenet_v2_1.0_224":
+            self.model = MobileNetV2ForImageClassification.from_pretrained(
+            self.model_name,
+            num_labels=len(self.labels_dict),
+            id2label=self.id2label,
+            label2id=self.label2id,
+            ignore_mismatched_sizes=True
+            ).to(self.device)
+            self.model.classifier = torch.nn.Sequential(
+                torch.nn.Dropout(p=0.2),  
+                torch.nn.Linear(in_features=1280, out_features=len(self.labels_dict)) 
+            )
+
+    def compute_metrics(self, p):
+        predictions = np.argmax(p.predictions, axis=1)
+        metric = load("f1")
+        return metric.compute(predictions=predictions, references=p.label_ids)
+    
+    def initialize_pipeline(self):
+        # Load augmented dataset
+        ds = self.create_augmented_dataset()
+        # Prepare model
+        self.prepare_model()
+        # Preprocess dataset
+        prepared_ds = ds.with_transform(self.transform)
+        # Training arguments
+        training_args = TrainingArguments(
+            output_dir=self.outputdir,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=16,
+            gradient_accumulation_steps=2,
+            num_train_epochs=4,
+            learning_rate=5e-5,
+            lr_scheduler_type="cosine_with_restarts",
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            logging_dir="./logs",
+            logging_steps=50,
+            save_total_limit=2,
+            load_best_model_at_end=True,
+            metric_for_best_model="accuracy",
+            remove_unused_columns=False,
+            fp16=torch.cuda.is_available(),
+            dataloader_pin_memory=False,
+            report_to=["none"],
+        )
+        # Initialize Trainer
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            data_collator=self.collate_fn,
+            train_dataset=prepared_ds["train"], 
+            eval_dataset=prepared_ds["test"],
+            compute_metrics=self.compute_metrics,
+        )
+        return trainer
+
+
+# Train the model
+try:
+    # Initialize pipeline
+    pipeline = PipelineTrain(model_name = "google/vit-base-patch16-224-in21k",
+                    dataset_path = Path(__file__).parent.parent / "augmented_dataset_class" , 
+                    labels_dict = "cropped-dataset-class/articles_train",
+                    outputdir="./vit-class")
+    trainer = pipeline.initialize_pipeline()
+    train_results = trainer.train()
+    print("Training complete.")
+except RuntimeError as e:
+    print(f"RuntimeError: {e}")
+    print("Model device:", next(pipeline.model.parameters()).device)
+    sample_batch = next(iter(trainer.get_train_dataloader()))
+    for k, v in sample_batch.items():
+        if isinstance(v, torch.Tensor):
+            print(f"{k}: shape={v.shape}, device={v.device}")
